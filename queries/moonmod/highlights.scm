172b7718d13f7d60e6f5942e39ac820db6dc4887
Tony Fettes <tonyfettes@tonyfettes.com>
2023-10-07 22:55:15 +0800

    add highlights to moon{mod,pkg}

---
+++ queries/moonmod/highlights.scm
@@ -0,0 +1,6 @@
+[
+  "module"
+  "require"
+] @keyword
+
+(module_path) @text.uri
---
+++ queries/moonpkg/highlights.scm
@@ -0,0 +1,7 @@
+[
+  "import"
+  "package"
+] @include
+
+(import_spec (path) @text.uri)
+(package_clause (name) @namespace)
