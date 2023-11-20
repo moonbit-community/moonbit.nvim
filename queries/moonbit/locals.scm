f0bef19410258195cafd2a8f4543a115e41eca50
Tony Fettes <tonyfettes@tonyfettes.com>
2023-11-21 01:04:04 +0800

    feat: locals

---
+++ queries/moonbit/locals.scm
@@ -0,0 +1,37 @@
+;; Definitions
+
+; Functions
+
+(function_definition (function_identifier) @definition.function)
+
+; Variables
+
+(value_definition (lowercase_identifier) @definition.var)
+(parameter (lowercase_identifier) @definition.parameter)
+(let_expression (pattern (lowercase_identifier) @definition.var))
+(var_expression (lowercase_identifier) @definition.var)
+
+; Types
+
+(struct_definition (identifier) @definition.type)
+(enum_definition (identifier) @definition.enum)
+(type_definition (identifier) @definition.type)
+(type_identifier) @definition.type
+
+;; References
+
+; Values
+(qualified_identifier) @reference
+
+; Types
+(qualified_type_identifier) @reference
+
+;; Scopes
+
+[
+ (structure)
+ (function_definition)
+ (anonymous_lambda_expression)
+ (named_lambda_expression)
+ (block_expression)
+] @scope
