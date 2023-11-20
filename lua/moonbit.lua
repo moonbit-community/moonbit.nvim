2698704baee66ee5c2d236737dab3c7918e63f3e
Tony Fettes <tonyfettes@tonyfettes.com>
2023-11-21 00:09:48 +0800

    feat: diagnostic through nvim-lint

--- lua/moonbit.lua
+++ lua/moonbit.lua
@@ -6,5 +6,26 @@ return {
     if has_treesitter and enable then
       require'moonbit.treesitter'.setup(treesitter_opts)
     end
+
+    local has_lint, lint = pcall(require, 'lint')
+    if has_lint then
+      lint.linters.moon = {
+        cmd = 'moon',
+        stdin = false,
+        append_fname = false,
+        stream = 'stdout',
+        ignore_exitcode = true,
+        args = { 'check' },
+        parser = require'lint.parser'.from_errorformat[[%E%f:%l:%c-%e:%k\ %m,%C\ %m,%-G%.%#]]
+      }
+      lint.linters_by_ft = {
+        moonbit = { 'moon' }
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require'lint'.try_lint()
+        end,
+      })
+    end
   end
 }
