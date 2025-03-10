85131274cbd765fbf3aae09ccc244e4922040d1e
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-10 12:57:06 +0800

    fix: subscribe to moon.{mod,pkg}.json

--- lua/moonbit.lua
+++ lua/moonbit.lua
@@ -128,61 +128,70 @@ return {
     end
 
     if opts.lsp ~= false then
+      local function on_attach(ev)
+        setup_toggle_multiline_string(ev.buf)
+        vim.lsp.start(vim.tbl_deep_extend("keep", opts.lsp or {}, {
+          name = 'moonbit-lsp',
+          cmd = { 'moonbit-lsp' },
+          root_dir = vim.fs.root(ev.buf, { 'moon.mod.json' }),
+          commands = {
+            ['moonbit-lsp/test'] = function(command, ctx)
+              local arguments = command.arguments[1]
+              local stdout = vim.uv.new_pipe()
+              local stderr = vim.uv.new_pipe()
+              local args = {
+                'test',
+                '--target=' .. arguments.backend,
+                '-p',
+                arguments.pkgPath,
+                '-f',
+                arguments.fileName,
+                '-i',
+                tostring(arguments.index),
+              }
+              if arguments.update then
+                table.insert(args, '-u')
+              end
+              local handle, pid = vim.uv.spawn('moon', {
+                args = args,
+                cwd = arguments.cwdUri:sub(7, -1),
+                stdio = { nil, stdout, stderr }
+              }, function()
+                stdout:close()
+                stderr:close()
+                vim.schedule(function()
+                  vim.api.nvim_buf_call(ev.buf, function()
+                    vim.cmd [[edit]]
+                  end)
+                end)
+                -- print('exit code', code)
+              end)
+              vim.uv.read_start(stdout, function(err, data)
+                assert(not err, err)
+                if not data then
+                  return
+                end
+                local trimmed = trim(data)
+                if trimmed ~= "" then
+                  print(trimmed)
+                end
+              end)
+            end,
+          },
+        }))
+      end
+
+      local moonbit_lsp_group = vim.api.nvim_create_augroup('MoonBitLsp', { clear = true });
+
       vim.api.nvim_create_autocmd('FileType', {
         pattern = 'moonbit',
-        group = vim.api.nvim_create_augroup("moonbit_lsp", { clear = true }),
-        callback = function(ev)
-          setup_toggle_multiline_string(ev.buf)
-          vim.lsp.start(vim.tbl_deep_extend("keep", opts.lsp or {}, {
-            name = 'moonbit-lsp',
-            cmd = { 'moonbit-lsp' },
-            root_dir = vim.fs.root(ev.buf, { 'moon.mod.json' }),
-            commands = {
-              ['moonbit-lsp/test'] = function(command, ctx)
-                local arguments = command.arguments[1]
-                local stdout = vim.uv.new_pipe()
-                local stderr = vim.uv.new_pipe()
-                local args = {
-                  'test',
-                  '--target=' .. arguments.backend,
-                  '-p',
-                  arguments.pkgPath,
-                  '-f',
-                  arguments.fileName,
-                  '-i',
-                  tostring(arguments.index),
-                }
-                if arguments.update then
-                  table.insert(args, '-u')
-                end
-                local handle, pid = vim.uv.spawn('moon', {
-                  args = args,
-                  cwd = arguments.cwdUri:sub(7, -1),
-                  stdio = { nil, stdout, stderr }
-                }, function()
-                  stdout:close()
-                  stderr:close()
-                  vim.schedule(function()
-                    vim.api.nvim_buf_call(ev.buf, function()
-                      vim.cmd [[edit]]
-                    end)
-                  end)
-                  -- print('exit code', code)
-                end)
-                vim.uv.read_start(stdout, function(err, data)
-                  assert(not err, err)
-                  if not data then
-                    return
-                  end
-                  local trimmed = trim(data)
-                  if trimmed ~= "" then
-                    print(trimmed)
-                  end
-                end)
-              end,
-            },
-          }))
-        end,
+        group = moonbit_lsp_group,
+        callback = on_attach,
+      })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+        pattern = { '*.mbt', 'moon.pkg.json', 'moon.mod.json', },
+        group = moonbit_lsp_group,
+        callback = on_attach,
       })
     end
   end
