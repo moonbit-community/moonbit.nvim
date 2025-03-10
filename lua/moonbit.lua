b15ff7efa38ba00b320d1b05585d04b4890260a6
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-10 13:20:30 +0800

    fix: find lsp-server.js

--- lua/moonbit.lua
+++ lua/moonbit.lua
@@ -105,6 +105,8 @@ local function setup_toggle_multiline_string(buffer)
   })
 end
 
+local path_sep = package.config:sub(1, 1)
+
 return {
   api = {
     toggle_multiline_string_operatorfunc = function(type)
@@ -127,12 +129,29 @@ return {
       require("plenary.filetype").add_file("moonbit")
     end
 
+    ---@return string
+    local function find_lsp_cmd()
+      local moon_home = vim.env["MOON_HOME"]
+      if moon_home == nil then
+        local home = vim.env["HOME"]
+        if home == nil then
+          home = '~'
+        end
+        moon_home = home .. path_sep .. '.moon'
+      end
+      local lsp_server_path = vim.fn.resolve(moon_home .. path_sep .. 'bin' .. path_sep .. 'lsp-server.js')
+      if vim.fn.executable(lsp_server_path) ~= 0 then
+        return lsp_server_path
+      end
+      return 'moonbit-lsp'
+    end
+
     if opts.lsp ~= false then
       local function on_attach(ev)
         setup_toggle_multiline_string(ev.buf)
         vim.lsp.start(vim.tbl_deep_extend("keep", opts.lsp or {}, {
           name = 'moonbit-lsp',
-          cmd = { 'moonbit-lsp' },
+          cmd = { find_lsp_cmd() },
           root_dir = vim.fs.root(ev.buf, { 'moon.mod.json' }),
           commands = {
             ['moonbit-lsp/test'] = function(command, ctx)
@@ -181,7 +200,7 @@ return {
         }))
       end
 
-      local moonbit_lsp_group = vim.api.nvim_create_augroup('MoonBitLsp', { clear = true });
+      local moonbit_lsp_group = vim.api.nvim_create_augroup('moonbit.lsp', { clear = true });
 
       vim.api.nvim_create_autocmd('FileType', {
         pattern = 'moonbit',
