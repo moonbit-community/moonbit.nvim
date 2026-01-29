7ce71b27416979f32077790264905c63b090fbbc
Binh Tran <binhtd.dev@gmail.com>
2026-01-29 13:50:43 +0700

    fix(treesitter): adapt main branch

--- lua/moonbit/treesitter.lua
+++ lua/moonbit/treesitter.lua
@@ -1,51 +1,22 @@
-local install_infos = {
-  ['moonbit'] = {
-    url = 'https://github.com/moonbitlang/tree-sitter-moonbit',
-    revision = 'd8e16ae8edd42a62f8e45e1a3b621f9c1b2661fc',
-    files = { 'src/parser.c', 'src/scanner.c' },
-    branch = 'main',
-  },
-}
-
-local function get_installed_revision(utils, configs, lang)
-  local lang_file = utils.join_path(configs.get_parser_info_dir(), lang .. ".revision")
-  if vim.fn.filereadable(lang_file) == 1 then
-    return vim.fn.readfile(lang_file)[1]
-  end
-end
-
-local function needs_update(utils, configs, lang)
-  local installed_revision = get_installed_revision(utils, configs, lang)
-  if not installed_revision then
-    return true
-  end
-  local info = install_infos[lang]
-  return installed_revision ~= info.revision
-end
-
 return {
   setup = function(opts)
-    local auto_install = opts.auto_install or true
-    local has_treesitter, parsers = pcall(require, 'nvim-treesitter.parsers')
-    if not has_treesitter then return end
-    local utils = require "nvim-treesitter.utils"
-    local configs = require "nvim-treesitter.configs"
-    local install = require "nvim-treesitter.install"
-    local update = install.update {}
-    local uninstall = install.uninstall
-
-    for filetype, install_info in pairs(install_infos) do
-      parsers.get_parser_configs()[filetype] = {
-        filetype = filetype,
-        install_info = install_info,
-      }
-      if auto_install then
-        if not parsers.has_parser(filetype) or needs_update(utils, configs, filetype) then
-          uninstall(filetype)
-          update(filetype)
-        end
-      end
+    local parser_name = 'moonbit'
+    vim.api.nvim_create_autocmd('User', { pattern = 'TSUpdate',
+      callback = function()
+        require('nvim-treesitter.parsers')[parser_name] = {
+          tier = 2,
+          install_info = {
+            url = 'https://github.com/moonbitlang/tree-sitter-moonbit',
+            revision = 'd8e16ae8edd42a62f8e45e1a3b621f9c1b2661fc',
+            files = { 'src/parser.c', 'src/scanner.c' },
+            branch = 'main',
+          },
+        }
+      end})
+    vim.treesitter.language.register(parser_name, { 'mbt' })
+    local auto_install =  opts.auto_install or opts.auto_install == nil
+    if auto_install then
+      require'nvim-treesitter'.install { parser_name }
     end
   end,
-
 }
